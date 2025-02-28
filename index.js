33ae95efc02f572b1e765262cc434cb43814a624
sww6246 <sam.weber7@gmail.com>
2025-02-28 09:27:44 -0700

    2nd HW Commit

---
+++ index.js
@@ -0,0 +1,32 @@
+let flavorOrder = prompt("Enter your ice cream flavor orders, separated by commas");
+console.log(flavorOrder);
+
+let flavorArr = flavorOrder.split(",");
+console.log(flavorArr)
+
+function frequency (array, item) {
+    let count = 0;
+    for (let i=0; i< array.length; i++) {
+        if (array[i] === item) {
+            count++;
+        }
+    }
+    return count;
+}
+
+const flavor1 = "vanilla";
+const flavor2 = "coffee";
+const flavor3 = "strawberry";
+
+let vanillaFreq = (frequency(flavorArr, flavor1));
+let coffeeFreq = (frequency(flavorArr, flavor2));
+let strawberryFreq = (frequency(flavorArr, flavor3));
+
+const orderFreq = {
+    vanilla: vanillaFreq,
+    coffee: coffeeFreq,
+    strawberry: strawberryFreq
+}
+
+
+console.table(orderFreq)
